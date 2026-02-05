a71c2762af1f2ab4abd2adc233286ef8105f96cc
youG <uno_Snowy_Owl@outlook.com>
2026-02-05 15:47:46 +0800

    gitignore文件测试

---
+++ .idea/fileTemplates/Cpp文件模板.cpp
@@ -0,0 +1,22 @@
+#set( $mastername = "youg" )
+#set( $COMPANY = "公司名称替换" )
+#set( $USAGE_NOTES = "使用方法补充" )
+#set( $DESCRIPTION = "文件英文描述补充" )
+#set( $DESCRIPTION_CN = "文件中文描述补充" )
+/**
+  ****************************(C) COPYRIGHT ${YEAR} ${COMPANY}****************************
+  * @file       ${FILE_NAME}
+  * @brief      ${DESCRIPTION}
+  *             ${DESCRIPTION_CN}
+  * @note       
+  * @history
+  *  Version    Date            Time            Author          Modification
+  *  V1.0.0     ${MONTH}-${DAY}-${YEAR}     ${TIME}            ${mastername}            1. 初始版本
+  *
+  @verbatim
+  ==============================================================================
+    ${USAGE_NOTES}
+  ==============================================================================
+  @endverbatim
+  ****************************(C) COPYRIGHT ${YEAR} ${COMPANY}****************************
+  */
